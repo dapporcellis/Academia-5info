1f1b67ff916c79fefe4294fe43a95931c5e96f2b
Diego de Abreu Porcellis <dapporcellis@hotmail.com>
2024-06-05 08:52:59 -0300

    passport

---
+++ config/passport.js
@@ -0,0 +1,45 @@
+import passport from 'passport'
+import LocalStrategy from 'passport-local'
+import Usuario from '../models/Usuario.js'
+
+passport.use(new LocalStrategy({
+    passReqToCallback: true, 
+    usernameField: 'email',
+    passwordField: 'senha',
+}, async function verify(req, username, password, cb) {
+    let usuario = await Usuario.findOne({
+        'email': username
+    })    
+    
+    if (!usuario) {        
+        return cb(null, false, 'Email não encontrado.')
+    } else if (usuario.senha != password) {
+        console.log('Erro de senha')
+        return cb(null, false, 'Senha incorreta.')
+    } else {
+        console.log('Passou')
+        return cb(null, usuario);
+    }
+
+}));
+
+passport.serializeUser(function (user, cb) {
+    process.nextTick(function () {
+        cb(null, {
+            id: user.id,
+            nome: user.nome,
+            email: user.email,
+            foto: user.foto,
+            admin: user.admin
+        });
+    });
+});
+
+passport.deserializeUser(function (user, cb) {
+    process.nextTick(function () {
+        return cb(null, user);
+    });
+});
+
+
+export default passport
